109b8fc9b86de2b6f31e652652a63181f9ea3412
mychelscott <scottmychel@gmail.com>
2023-11-19 11:55:03 -0800

    new constructor (id) parameter added

--- studentclass.js
+++ studentclass.js
@@ -1,14 +1,14 @@
 //class with constructor parameters
 class Student {
-    constructor (name, age, major) {
-        this.name = name;
+    constructor (id, age, major) {
+        this.id = id;
         this.age = age;
         this.major = major;
     }
 }
 
 //instance of the student class
-const student1 = new Student ("Mychel Scott", 27, "Web Development");
+const student1 = new Student ("33369", 27, "Web Development");
 
 //logged variable
 console.log(student1);
