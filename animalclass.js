d11eadfbdd0b18432fe93e889db55e073c37843e
mychelscott <scottmychel@gmail.com>
2023-11-19 13:00:38 -0800

    class that extends Animal added

--- animalclass.js
+++ animalclass.js
@@ -1,12 +1,12 @@
 class Animal {
-    constructor (type, height, weight){
-    this.type = type;
+    constructor (name, height, weight){
+    this.name = name;
     this.height = height;
     this.weight = weight;
 }
 
-printType() {
-    console.log(`The type of this animal is ${this.type}`);
+printName() {
+    console.log(`The name of this animal is ${this.name}`);
 }
     showHeight() {
         console.log(`The height of this animal is ${this.height} ft`);
@@ -15,7 +15,24 @@ printType() {
         console.log(`The weight of this animal is ${this.weight}lbs`);
     }
 }
-const ringedSeal = new Animal("seal", 5, 120);
 
-// print the type of the ringedSeal
-ringedSeal.printType();
+
+class SeaCreature extends Animal {
+    constructor (name, height, weight, canSwim) {
+        super(name, height, weight);
+        this.canSwim = canSwim;
+    }
+    swim() {
+        if (this.canSwim) {
+            console.log(`${this.name} is swimming.`);
+        }
+        else {
+            console.log(`${this.name} cannot swim.`);
+        }
+    }
+}
+const ringedSealSeaCreature = new SeaCreature("seal", 5, 120, true);
+
+ringedSealSeaCreature.printName();
+
+ringedSealSeaCreature.swim();
