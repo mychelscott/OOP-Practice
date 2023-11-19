53c56bc2a06b15b86521beab8bb0b7d9567c1f49
mychelscott <scottmychel@gmail.com>
2023-11-19 12:40:49 -0800

    syntax errors fixed

--- animalclass.js
+++ animalclass.js
@@ -1,12 +1,12 @@
 class Animal {
-    constructor (type, height, weight);
+    constructor (type, height, weight){
     this.type = type;
     this.height = height;
     this.weight = weight;
 }
 
 printType() {
-    console.log(`The type of this animal is` ${this.type}`);
+    console.log(`The type of this animal is ${this.type}`);
 }
     showHeight() {
         console.log(`The height of this animal is ${this.height} ft`);
@@ -17,5 +17,5 @@ printType() {
 }
 const ringedSeal = new Animal("seal", 5, 120);
 
-//print the type of the ringedSeal
+// print the type of the ringedSeal
 ringedSeal.printType();
